058f48dd50efd4e3eb7ed02bc8348db100bafa34
Rebeca Motta <beca_bh@me.com>
2020-12-01 16:34:52 -0300

    Melhoria Ex2

--- Exercicios_Desafio_1/2.dart
+++ Exercicios_Desafio_1/2.dart
@@ -1,15 +1,16 @@
 void main(List<String> args) {
-  List<String> nomes = List();
-  nomes.add('Rebeca');
-  nomes.add('Marina');
-  nomes.add('Lais');
-  nomes.add('Mariana');
-  nomes.add('Luiza');
-  nomes.add('Pedro');
-  nomes.add('José');
-  nomes.add('João');
-  nomes.add('Mateus');
-  nomes.add('Luiz');
+  List<String> nomes = [
+    "Rebeca",
+    "Marina",
+    "Lais",
+    "Mariana",
+    "Luiza",
+    "Pedro",
+    "José",
+    "João",
+    "Mateus",
+    "Fernando"
+  ];
 
   for (var nome in nomes) {
     print('Seja bem vindo ${nome.toUpperCase()}');
