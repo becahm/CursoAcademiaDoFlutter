e7df510702de5d97cfb79fabfa7cb6e77da283bf
Rebeca Motta <beca_bh@me.com>
2020-12-01 20:46:31 -0300

    troca de pasta

--- Exercicios_Desafio_1/1.dart
+++
@@ -1,6 +0,0 @@
-void main(List<String> args) {
-  String nome =
-      'Pedro de Alcântara Francisco Antônio João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon';
-  print(nome);
-  print('Esse nome tem ${nome.length} caracteres');
-}
--- Exercicios_Desafio_1/3.dart
+++
@@ -1,7 +0,0 @@
-void main(List<String> args) {
-  String nome =
-      'Pedro de Alcântara Francisco Antônio João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon';
-
-  var nomes = nome.split(" ");
-  print('Esse nome possui ${nomes.length} nomes');
-}
