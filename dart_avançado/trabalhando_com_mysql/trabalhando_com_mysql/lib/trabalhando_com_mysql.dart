604885fb9234fde0e35b6670f1119fd82d3de85a
Rebeca Motta <beca_bh@me.com>
2020-12-07 10:53:04 -0300

    desafio_dart_avancado

--- dart_avançado/trabalhando_com_mysql/trabalhando_com_mysql/lib/trabalhando_com_mysql.dart
+++ dart_avançado/trabalhando_com_mysql/trabalhando_com_mysql/lib/trabalhando_com_mysql.dart
@@ -1,6 +1,8 @@
 import 'package:mysql1/mysql1.dart';
 
-void run() {}
+void run() {
+  cadastrarAluno();
+}
 
 Future<MySqlConnection> getConnection() async {
   var settings = ConnectionSettings(
@@ -10,4 +12,22 @@ Future<MySqlConnection> getConnection() async {
       password: 'root',
       db: 'curso_dart_flutter');
   return await MySqlConnection.connect(settings);
+}
+
+Future<void> cadastrarAluno() async {
+  print("Cadastrando Aluno");
+  var connection = await getConnection();
+  var result = await connection
+      .query('insert aluno(id_aluno, nome) values(null, ?)', ['Rebeca Motta']);
+  await connection.close();
+  print('Aluno cadastrado');
+}
+
+Future<void> atualizandoAluno() async {
+  print("Atualizando Aluno");
+  var connection = await getConnection();
+  var result = await connection
+      .query('update aluno(id_aluno, nome) values(null, ?)', ['Rebeca Motta']);
+  await connection.close();
+  print('Aluno atualizado');
 }
